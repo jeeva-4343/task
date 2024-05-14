abf96d69e5f53c6568001178fe5487d78eeb4582
jeeva-4343 <jeevanehru4343@gmail.com>
2024-05-14 15:24:26 +0530

    update section-3

--- src/Html/Section-3.jsx
+++ src/Html/Section-3.jsx
@@ -17,7 +17,7 @@ function  Section3() {
                 <div className='Section3-head'>We Provides Best Features <div>for Customers</div> </div>
             </Row>
             <Row className='section3-card-row justify-content-center'> {/* Center the card row */}
-                <Col lg={3} xl={2} md={5} className='mb-4'> {/* Adjust the column size for responsiveness */}
+                <Col lg={3} xl={2} md={5} sm={8} className='mb-4'> {/* Adjust the column size for responsiveness */}
                     <Card className='card-main-same'>
                         <div className='icon-circle-div'><RiFileCodeLine className='icon-circle' style={{ backgroundColor: '#f9e9e9', color: '#9e1c1a' }} size={100} /></div>
                         <div class="wave-container">
@@ -32,7 +32,7 @@ function  Section3() {
                         </div>
                     </Card>
                 </Col>
-                <Col lg={3} xl={2} md={5} className='mb-4'>
+                <Col lg={3} xl={2} md={5} sm={8} className='mb-4'>
                     <Card className='card-main-same'>
                         <div className='icon-circle-div'><BsClipboardData className='icon-circle' style={{ backgroundColor: '#f8f1e7', color: '#9a5d28' }} size={100} /></div>
                         <div class="wave-container">
@@ -46,7 +46,7 @@ function  Section3() {
                         </div>
                     </Card>
                 </Col>
-                <Col lg={3} xl={2} md={5} className='mb-4'>
+                <Col lg={3} xl={2} md={5} sm={8} className='mb-4'>
                     <Card className='card-main-same'>
                         <div className='icon-circle-div'><PiFileLockDuotone className='icon-circle' style={{ backgroundColor: '#e7f6f6', color: '#5a939f' }} size={100} /></div>
                         <div class="wave-container">
@@ -60,7 +60,7 @@ function  Section3() {
                         </div>
                     </Card>
                 </Col>
-                <Col lg={3} xl={2} md={5} className='mb-4'>
+                <Col lg={3} xl={2} md={5} sm={8} className='mb-4'>
                     <Card className='card-main-same'>
                         <div className='icon-circle-div'><MdOutlineContactSupport className='icon-circle' style={{ backgroundColor: '#f4e7f7', color: '#c739d7' }} size={100} /></div>
                         <div class="wave-container">
